0079865e98945f3ecbe7d6ad8369cd2bdc384823
Zaheerabbas <zabbas092002@gmail.com>
2025-07-03 21:42:12 +0500

    add the cards into courses component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { CourseProvider } from "./context/CourseContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CourseProvider>
+        <App />
+      </CourseProvider>
     </BrowserRouter>
   </React.StrictMode>
+
 );
